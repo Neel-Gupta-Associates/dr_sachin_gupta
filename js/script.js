e2067c117b04fa05b1478f60e54058c8bfa73c69
neelgupta123 <75940404+neelgupta123@users.noreply.github.com>
2023-03-31 13:10:16 +0530

    made localstorage for theme

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-sun = document.querySelector(".sun");
-moon = document.querySelector(".moon");
-html = document.querySelector(".html");
+var sun = document.querySelector(".sun");
+var moon = document.querySelector(".moon");
+var html = document.querySelector(".html");
+var themeVal = html.getAttribute('data-bs-theme');
 
 moon.addEventListener('click', function(){
     html.setAttribute("data-bs-theme", "dark");
@@ -9,3 +10,12 @@ moon.addEventListener('click', function(){
 sun.addEventListener('click', function(){
     html.setAttribute("data-bs-theme", "light");
 })
+
+function getVal(){
+    var themeVal = html.getAttribute('data-bs-theme');
+    localStorage.setItem('theme', themeVal);
+}
+setInterval(getVal, 50);
+
+var theme = localStorage.getItem('theme');
+html.setAttribute("data-bs-theme", theme);
